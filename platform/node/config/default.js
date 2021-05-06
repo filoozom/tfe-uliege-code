eeb1c1cc27624cb8a5fa408665018c18912f0bb2
Philippe Schommers <philippe@schommers.be>
2021-05-06 23:41:41 +0200

    Add ethers keys

--- platform/node/config/default.js
+++ platform/node/config/default.js
@@ -6,6 +6,8 @@ module.exports = {
         projectId: "7232dd50de3c49e1842650219cc1626e",
         projectSecret: "f19401e0d77643c5ad1da681b96d1f0a",
       },
+      alchemy: "GPR_qF9l_vj-iDb1Kdg3cjDBB-ktJ7Lt",
+      etherscan: "VSPRPIV1ZMDJNSH59U16883KTEU7PMB4NN",
     },
   },
 
